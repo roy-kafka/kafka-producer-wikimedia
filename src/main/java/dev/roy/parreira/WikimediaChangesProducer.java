85f0f87e537b7cd754abb58e7258a66981663bc4
Roy Parreira <royparreira@gmail.com>
2022-09-17 11:27:23 -0300

    Adding dependencies

---
+++ src/main/java/dev/roy/parreira/WikimediaChangesProducer.java
@@ -0,0 +1,10 @@
+package dev.roy.parreira;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class WikimediaChangesProducer {
+  public static void main(String[] args) {
+    log.info("Hello world!!!");
+  }
+}
