39e4739201670d7b6b82edcb2720a1856459083f
Roy Parreira <royparreira@gmail.com>
2022-09-19 13:59:45 -0300

    Ensuring producer has safe configs

--- src/main/java/dev/roy/parreira/WikimediaChangesProducer.java
+++ src/main/java/dev/roy/parreira/WikimediaChangesProducer.java
@@ -5,6 +5,7 @@ import com.launchdarkly.eventsource.EventSource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
 import java.util.Properties;
@@ -12,7 +13,11 @@ import java.util.concurrent.TimeUnit;
 
 import static java.util.Objects.isNull;
 import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION;
+import static org.apache.kafka.clients.producer.ProducerConfig.RETRIES_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 @Slf4j
@@ -30,15 +35,30 @@ public class WikimediaChangesProducer {
 
     if (isNull(kafkaProducer)) {
 
-      Properties properties = new Properties();
-      properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-      properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-      properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+      Properties properties = setProducerProperties();
 
       kafkaProducer = new KafkaProducer<>(properties);
     }
 
     return kafkaProducer;
+  }
+
+  @NotNull
+  private static Properties setProducerProperties() {
+    Properties properties = new Properties();
+
+    // Basic Properties
+    properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+    properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+    // set safe producer configs (Kafka <= 2.8)
+    properties.setProperty(ENABLE_IDEMPOTENCE_CONFIG, "true");
+    properties.setProperty(ACKS_CONFIG, "all");
+    properties.setProperty(RETRIES_CONFIG, String.valueOf(Integer.MAX_VALUE));
+    properties.setProperty(MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+    return properties;
   }
 
   public void startProduceFromWikimedia() throws InterruptedException {
